02f2273c019032807e2644126ac186bbd994ac56
Sevastian Kozhunov <sky.seva@gmail.com>
2019-08-27 16:04:18 +0200

    Start map generation implementation

---
+++ New World Unity Project/Assets/Source/Battlefield/Map/Generation/ExperimentalMapGenerator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Unity.Mathematics;
+using NewWorld.Battlefield.Map;
+
+namespace NewWorld.Battlefield.Map.Generation {
+
+    public class ExperimentalMapGenerator : MapGenerator {
+
+        public override MapDescription Generate() {
+            throw new System.NotImplementedException();
+        }
+
+    }
+
+}
---
+++ New World Unity Project/Assets/Source/Battlefield/Map/Generation/MapGenerator.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using Unity.Mathematics;
+using NewWorld.Battlefield.Map;
+
+namespace NewWorld.Battlefield.Map.Generation {
+
+    public abstract class MapGenerator {
+
+        // Fields.
+
+        private int seed;
+        private float heightLimit;
+        private Vector2 size;
+
+
+        // Constructor.
+
+        public MapGenerator() {}
+
+
+        // Properties.
+
+        public int Seed {
+            get => seed;
+            set => seed = value;
+        }
+
+        public float HeightLimit {
+            get => heightLimit;
+            set => heightLimit = value;
+        }
+
+        public Vector2 Size {
+            get => size;
+            set => size = value;
+        }
+
+
+        // Methods.
+
+        public abstract MapDescription Generate();
+
+
+    }
+
+}
