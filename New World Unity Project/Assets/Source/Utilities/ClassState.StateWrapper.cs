c7135a6d3303b7f87fea2e689c796dbe2c8772d0
Sevastian Kozhunov <sky.seva@gmail.com>
2020-09-05 17:40:54 +0200

    Implement class state tool

---
+++ New World Unity Project/Assets/Source/Utilities/ClassState.StateWrapper.cs
@@ -0,0 +1,24 @@
+﻿namespace NewWorld.Utilities {
+
+    public partial class ClassState<TDelta> {
+        
+        // Wrapper.
+
+        public class StateWrapper : ClassWrapper<ClassState<TDelta>> {
+
+            // Constructor.
+
+            public StateWrapper(ClassState<TDelta> state) : base(state) {}
+
+
+            // Methods.
+
+            public StateWrapper Transit(out TDelta delta) => Wrapped.Transit(out delta)?.Wrapper; 
+
+
+        }
+
+
+    }
+
+}
---
+++ New World Unity Project/Assets/Source/Utilities/ClassState.cs
@@ -0,0 +1,40 @@
+﻿namespace NewWorld.Utilities {
+
+    public partial class ClassState<TDelta> {
+
+        // Fields.
+
+        private ClassState<TDelta> next = null;
+        private TDelta delta = default;
+        private readonly StateWrapper wrapper;
+
+
+        // Constructor.
+
+        public ClassState() {
+            wrapper = new StateWrapper(this);
+        }
+
+
+        // Properties.
+
+        public StateWrapper Wrapper => wrapper;
+
+
+        // Methods.
+
+        public ClassState<TDelta> Transit(out TDelta delta) {
+            delta = this.delta;
+            return next;
+        }
+
+        public ClassState<TDelta> BuildTransition(TDelta delta) {
+            this.delta = delta;
+            next = new ClassState<TDelta>();
+            return next;
+        }
+
+
+    }
+
+}
